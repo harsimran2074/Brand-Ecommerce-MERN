9f8de08185b4aff4c1c7363fc9b9f5ec798da295
harsimran2074 <harsimran2074handa@gmail.com>
2026-08-28 23:06:03 +0530

    random

--- backend/app.js
+++ backend/app.js
@@ -7,3 +7,10 @@ import productRoute from "./routes/product.js";
 import cartRoute from "./routes/cart.js";
 import orderRoute from "./routes/order.js";
 import stripeRoute from "./routes/stripe.js";
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
