21dd86dbb2067d6bb0012b6033add1871f1c44ec
harsimran2074 <harsimran2074handa@gmail.com>
2026-09-07 20:23:31 +0530

    user order page created successfully

--- admin/src/pages/order.jsx
+++ admin/src/pages/order.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Order = () => {
+    const [orderData, setOrderData] = useState([])
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const token = localStorage.getItem("token")
+    const showOrders = async () => {
+
+        try {
+            const res = await axios.get(backendUrl + "/api/order/list", { headers: { token } })
+            if (res.data.success) {
+                setOrderData(res.data.allorders)
+                console.log(res);
+            }
+        } catch (err) {
+            toast.error(err.message)
+        }
+    }
+
+    useEffect(() => {
+        showOrders()
+    }, [])
+
+
     return (
-        <div>Order</div>
+        <>
+            <p>orders</p>
+        </>
+
     )
 }
 
